eb9a5926d5e76be4633e74845d1f88a86ce75cab
Ashrith201216 <nnreddyshiv@gmail.com>
2024-07-04 13:13:55 -0400

    javascript_practice

---
+++ Greater.js
@@ -0,0 +1,10 @@
+
+let val1=39
+let val2=216
+
+if(val1 > 60 && val2 < 30 ){
+
+    console.log('is greater than ')
+}
+else{
+    console.log('is not greater than')}
---
+++ practice.js
@@ -0,0 +1,12 @@
+var x;
+var y;
+x = 12;
+y = 15;
+if (x > y)
+{  console.log(" X greater than y")}
+  
+else
+{ console.log("X is less than Y")}
+   ;
+
+
